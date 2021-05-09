b9d7356946adfdf3ea3ce5f356046436d62c57c5
Daniela <dg.gianora@gmail.com>
2021-05-09 21:24:03 +0200

    minor update for consistency

--- donation-coin/src/components/SearchBar.js
+++ donation-coin/src/components/SearchBar.js
@@ -1,18 +1,21 @@
-const SearchBar = ({ keyword, setKeyword }) => {
+const SearchBar = ({ keyword }) => {
     console.log(keyword)
 
     function onKeyUp(e) {
         if (e.charCode === 13) {
             console.log(e.target.value)
-        //   this.setState({ inputValue: e.target.value });
+        // TODO:
+        // (retrieve the donation from the blockchain and) save the result so that 
+        // the transaction component can dynamically update the its rendering
         }
     };
 
     return (
         <input
             value={keyword}
-            placeholder={"search donation"}
+            placeholder={"Track a donation"}
             onKeyPress={onKeyUp}
+            className="searchbar"
         />
     );
 }
