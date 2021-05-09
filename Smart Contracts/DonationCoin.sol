8b4c38b0e65aacec3deeeaf472ee84c20c208258
AbdlRahman Essa <abdlrahmanfathy2020@gmail.com>
2021-05-09 06:32:37 +0200

    bug fixes

--- Smart Contracts/DonationCoin.sol
+++ Smart Contracts/DonationCoin.sol
@@ -2,40 +2,46 @@ pragma solidity >=0.7.0 <0.9.0;
 
 
 // User the imports and their modifiers/ functions at a later stage.
-import "./ConvertLib.sol";
-import "./ownable.sol";
-import "./"
+//import "./ConvertLib.sol";
+//import "./ownable.sol";
 
 //using SafeMath for uint256;  
 //using SaFfeMath32 for uint32;
 //using SafeMath16 for uint16;
 
 
-contract DonationCoin{
+contract DonationCoin {
 	
+	// variable holding the address of the Smart Contract's Owner (Adminstrator)
+	address public owner;
 	
 	// mappings
-    mapping (string => address) public charityType; // Maps
-    mapping (address => uint) public totalDonations; // Mapping: number of donations to a donor's address
-    mapping (uint => address) public donationToDonator; // Mapping: donation to donor's address
+    mapping (string => address) public charityType;
+    mapping (uint => address) public donationsNumber; // Number of donations made by a donor
+    mapping (uint => address) public donationToDonator; //
 	
 	
 	
 	// events
 	event NewDonation(address indexed _from, address indexed _to, uint256 _value); // Fired when a donation is made
-    event NewDonor(string _id, address _address);  // Fired when a new donor is registered
+    event NewDonor(string _userName, address _address);  // Fired when a new donor is registered
 
+    uint transactionID; // Unique identifier for each  transactionID
+    
+    constructor() public {
+        transactionID = 0;
+    }
 
 	struct Donor
 	{
-		string id;
-		//uint balance;
 		address donor_address;
+		string  userName;
+		//uint amount;
 	}
 
 	Donor[] public donors; // list of donors
 
-	Struct Charity
+	struct Charity
 	{
 		string category;
 		string name;
@@ -46,34 +52,54 @@ contract DonationCoin{
 	
 	struct Donation
 	{
-	    uint256 id;
-		address msg.sender;
+	    uint id;
+		address _from;
+		address _to;
 		uint amount;
-		address Charity;
+	    string message; // if the user chooses not to send a message, an empty String should be sent to the function call.
 	}
 
 	Donation[] public donations; // list of all donations
 
 
 	// Register a new Donor
-	function _registerDonor(string memory _id, address _address) public
+	function _registerDonor(address _address, string memory _userName) public
 	{
-		donors.push(donor(_id, _address));
-		emit NewDonor(_id, _address);
+		donors.push(Donor(_address, _userName ));
+		emit NewDonor(_userName, _address);
 	}
 
 
-	function _makeDonation(address payable _receiver, uint _amount) public payable returns(uint){
+	function _makeDonation(address payable _receiver, uint _amount, string memory message) public payable returns(uint){
         address _from = msg.sender;
         //require (getBalanceInEth(msg.sender) > amount);
-		//sendViaTransfer(_receiver);
-		donations.push(donation(_from, _amount, _receiver));
+		sendViaTransfer(_receiver);
+		transactionID += 1;
+		donations.push(Donation(transactionID, _from, _receiver, _amount, message));
 		emit NewDonation(msg.sender, _receiver, _amount);
 	}
 	
-    function sendViaTransfer(address payable _to) public payable {
+	function donationOf(uint256 _id) external view returns (address) {
+        return donationToDonator[_id];
+  }
+	
+// 	function getDonations(address _address) public view{
+// 	    //return list of donations of an address
+// 	}
+
+// 	function getBalanceInEth(address addr) public view returns(uint){
+// 		return ConvertLib.convert(getBalance(addr),2);
+// 	}
+
+// 	function getBalance(address addr) public view returns(uint) {
+// 		return balances[addr];
+// 	}
+
+ 
+ function sendViaTransfer(address payable _to) public payable {
         // This function is no longer recommended for sending Ether.
-                    _to.transfer(msg.value);}
+        _to.transfer(msg.value);
+    }
 
     function sendViaSend(address payable _to) public payable {
         // Send returns a boolean value indicating success or failure.
@@ -90,21 +116,3 @@ contract DonationCoin{
     }
     
 }
-    // Return 
-	function donationOf(uint256 _id) external view returns (address) {
-        return donationToDonator[_id];
-  }
-	
-// 	function getDonations(address _address) public view{
-// 	    //return list of donations of an address
-// 	}
-
-	function getBalanceInEth(address addr) public view returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
-	}
-
-	function getBalance(address addr) public view returns(uint) {
-		return balances[addr];
-	}
-
- 
